33f41b9b71d50357faf5a7251b71b1daf1d21ced
Jakub-Kubior <255703@edu.p.lodz.pl>
2025-01-30 13:34:15 +0100

    Dodanie funkcji dodawania 01

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+int Policz(int a, int b)
+{
+    return a+b;
+}
+
 int main(void)
 {
     printf("Hello, World!\n");
+    printf("W pierwszym rachunku 2 + 3: %d",Policz(2,3));
     return 0;
 }
